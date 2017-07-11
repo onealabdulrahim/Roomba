ff4126da135959d87bafb212438b11e5e82d959e
Oneal A <oneal.abdul@gmail.com>
2017-07-11 12:05:28 -0500

    Clean up conventions

--- MainControl.py
+++ MainControl.py
@@ -8,23 +8,25 @@ import DeadReckon
 # global r is robot object from create library
 r = create.Create("COM4")
 
-XFinal = 0
-YFinal = 0
+# Final and initial values of (x, y) given as cartesian coordinates
+X_FINAL = 0
+Y_FINAL = 0
 X = -2
 Y = -1
-Beta = 0
+BETA = 0
 Um = 100
 LC = r.getSensor("LEFT_ENCODER")
 RC = r.getSensor("RIGHT_ENCODER")
 
+# tracking data & pathing in text file
 xf = open('xf', 'w')
 yf = open('yf', 'w')
 # Vf = open('Vf','w')
 # Wf = open('Wf','w')
 
-while X != XFinal and Y != YFinal:
-    (V, W) = ControllerRobust.Control(X, Y, Um, XFinal, YFinal)
-    (X, Y, Beta, LC, RC) = DeadReckon.Position(V, W, X, Y, Beta, LC, RC, r)
+while X != X_FINAL and Y != Y_FINAL:
+    (V, W) = ControllerRobust.Control(X, Y, Um, X_FINAL, Y_FINAL)
+    (X, Y, BETA, LC, RC) = DeadReckon.Position(V, W, X, Y, BETA, LC, RC, r)
     xf.write(str(X))
     xf.write('\n')
     yf.write(str(Y))
